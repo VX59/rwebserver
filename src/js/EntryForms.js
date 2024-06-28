85e404847094f7b0b54961b7d5178260742aa54c
rsenic <derosajb19@gmail.com>
2024-06-28 02:08:49 -0400

    adding directory system and endpoints

--- src/js/EntryForms.js
+++ src/js/EntryForms.js
@@ -17,18 +17,44 @@ function LoadForms() {
 
 const root = 'http://localhost:8081';
 
-export async function PageNavigator() {
-    const table = document.createElement("ul");
-
+export async function AudioPackageNavigator() {
+    const pacakge_table = document.createElement("div");
+    const file_table = document.createElement("div");
+    file_table.id = "file-table";
     // get packages here
     const response = await fetch(root+'/packages')
     .then((response) => response.json())
     .then((data) => {
         for (const pkg_name of data.Packages) {
-            const pkg = document.createElement("li");
+            const pkg = document.createElement("button");
             pkg.innerHTML = pkg_name;
-            table.appendChild(pkg);
+            pacakge_table.appendChild(pkg);
+
+            pkg.addEventListener("click", async function(){
+                file_table.innerHTML = "";
+                // get packages here
+                const headers = new Headers({
+                    'Content-Type': 'application/json',
+                    'package-name': pkg_name
+                })
+                const options = {
+                    method: 'GET',
+                    headers: headers
+                  };
+                const response = await fetch(root+'/content/',options)
+                .then((response) => response.json())
+                .then((data) => {
+                    for (const file_name of data.Packages) {
+                        const file = document.createElement("p");
+                        file.className = "audio-package-file";
+                        file.innerHTML = file_name;
+                        file_table.appendChild(file);
+                    }
+                });
+                document.body.append(file_table);
+            });
+
         }
     });
-    document.body.append(table);
+    document.body.append(pacakge_table);
 }
--- src/webserver.go
+++ src/webserver.go
@@ -32,26 +32,68 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s", p.Body)
 }
 
-type SoundGroups struct {
+type groups struct {
 	Name     string
 	Packages []string
 }
 
-func PackageHandler(w http.ResponseWriter, r *http.Request) {
+func PackageViewHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
 
 	// database query
-	groups := SoundGroups{"Sound Groups", []string{"sample beats", "sample instrumentals"}}
+
+	audio_packages_fp, err := os.Open("C:/Users/deros/rwebserver/audio-data-packages")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	audio_packages, err := audio_packages_fp.ReadDir(0)
+	audio_package_names := []string{}
+	for p := 0; p < len(audio_packages); p++ {
+		audio_package_names = append(audio_package_names, audio_packages[p].Name())
+	}
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(audio_package_names)
+
+	groups := groups{"packages", audio_package_names}
 
 	if err := json.NewEncoder(w).Encode(&groups); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
+func FileViewHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	target := r.Header.Get("package-name")
+	fmt.Println("target", target)
+	fp, err := os.Open("C:/Users/deros/rwebserver/audio-data-packages/" + target)
+	if err != nil {
+		fmt.Println(err)
+	}
+	audio_files, err := fp.ReadDir(0)
+	audio_file_names := []string{}
+	for p := 0; p < len(audio_files); p++ {
+		audio_file_names = append(audio_file_names, audio_files[p].Name())
+	}
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	groups := groups{"package-content", audio_file_names}
+	if err := json.NewEncoder(w).Encode(&groups); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+}
+
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("js/")))
-	http.HandleFunc("/packages/", PackageHandler)
+	http.HandleFunc("/packages/", PackageViewHandler)
+	http.HandleFunc("/content/", FileViewHandler)
 	http.HandleFunc("/view/", viewHandler)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 
